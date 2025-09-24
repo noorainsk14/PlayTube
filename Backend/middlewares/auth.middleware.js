9305edb2a883ce6d19973b9b738af0abfbb51bf9
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-09-24 17:18:20 +0530

    feat: creted auth middleware

---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,31 @@
+import { User } from "../models/User.model";
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (token) {
+      throw new ApiError(404, "Unauthorize access");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid accces token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "verifyJwt error");
+  }
+});
