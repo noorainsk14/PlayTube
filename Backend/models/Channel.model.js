461acb949b0982e1d803f8e3d6d2666f7cd961fb
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-10-02 15:35:40 +0530

    feat: created Channel model.

---
+++ Backend/models/Channel.model.js
@@ -0,0 +1,69 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const channelSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: [true, "Channel name is required"],
+      unique: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    profileImg: {
+      type: String,
+      default: "",
+    },
+    subscribers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    shorts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Short",
+      },
+    ],
+    playlist: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Playlist",
+      },
+    ],
+    communityPost: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+channelSchema.pre("save", function (next) {
+  this.name = this.name.trim();
+  next();
+});
+
+export const Channel = model("Channel", channelSchema);
