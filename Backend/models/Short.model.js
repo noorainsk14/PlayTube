27d33263c150b1472ba573782446a99ab80fc157
Noorain Shaikh <noorainshaikh308@gmail.com>
2025-10-05 13:59:30 +0530

    feat: Add video and short models.

---
+++ Backend/models/Short.model.js
@@ -0,0 +1,82 @@
+import mongoose, { Schema, model, Types } from "mongoose";
+
+const replySchema = new Schema({
+author:{
+        type:Types.ObjectId,
+        ref: "User"
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date
+    }
+},{_id:true})
+
+const commentSchema = new Schema({
+    author:{
+        type:Types.ObjectId,
+        ref: "User"
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    replies:{replySchema},
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date
+    }
+},{_id:true})
+
+const shortSchema = new Schema({
+    channel:{
+        type:Types.ObjectId,
+        ref:"Channel",
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        default:""
+    },
+    shortUrl:{
+        type:String,
+        required:true
+    },
+    tags:{
+        type:String,
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    likes:[{
+        type:Types.ObjectId,
+        ref:"User",
+    }],
+    disLikes:[{
+        type:Types.ObjectId,
+        ref:"User",
+    }],
+    savedBy:[{
+            type:Types.ObjectId,
+            ref:"User",
+        }],
+    comments:{commentSchema}
+
+
+}, {timestamps:true})
+
+export const Short = model("Short", shortSchema)
---
+++ Backend/models/Video.model.js
@@ -0,0 +1,86 @@
+import mongoose, { Schema, model, Types } from "mongoose";
+
+const replySchema = new Schema({
+author:{
+        type:Types.ObjectId,
+        ref: "User"
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date
+    }
+},{_id:true})
+
+const commentSchema = new Schema({
+    author:{
+        type:Types.ObjectId,
+        ref: "User"
+    },
+    message:{
+        type:String,
+        required:true
+    },
+    replies:{replySchema},
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt:{
+        type: Date
+    }
+},{_id:true})
+
+const videoSchema = new Schema({
+    channel:{
+        type:Types.ObjectId,
+        ref:"Channel",
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        default:""
+    },
+    videoUrl:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    tags:{
+        type:String,
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    likes:[{
+        type:Types.ObjectId,
+        ref:"User",
+    }],
+    disLikes:[{
+        type:Types.ObjectId,
+        ref:"User",
+    }],
+    savedBy:[{
+            type:Types.ObjectId,
+            ref:"User",
+        }],
+    comments:{commentSchema}
+
+
+}, {timestamps:true})
+
+export const Video = model("Video", videoSchema)
